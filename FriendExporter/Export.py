6bc7e2aab4d99b0e2ebb1c99587ac9a071f0e53e
Soylol <71528504+Soylol@users.noreply.github.com>
2021-04-09 11:39:37 -0600

    Add files via upload

---
+++ FriendExporter/Export.py
@@ -0,0 +1,46 @@
+import requests
+import json
+import io
+
+with open('config.json') as r:
+	config = json.load(r)
+
+Token = config.get('DiscordToken')
+
+headers = {
+	'Authorization': Token,
+	'User-Agent': 'Samsung/Fridge 6.9/9.6'
+}
+
+Output = []
+
+def export_friends():
+	friend_request_url = 'https://discord.com/api/v8/users/@me/relationships'
+	friend_request = requests.get(url=friend_request_url, headers=headers)
+	friend_big_data = friend_request.json()
+	return friend_big_data
+
+def export_to_json(uid, type, nickname, username, discriminator):
+	friend_dict = {
+		"User ID" : uid,
+		"Type" : type,
+		"Nickname" : nickname,
+		"Username": username,
+		"Discriminator": discriminator
+		}
+	json_object = json.dumps(friend_dict, indent = 4)
+	with io.open('Exported/Exported.json', 'a', encoding='utf-8') as f:
+		Output.append(friend_dict)
+		return Output
+		#f.write(str(json.dumps(Output, ensure_ascii=False)))
+		#f.write(u'\n')
+
+def sort_friends():
+	friend_data = export_friends()
+	for count, friend in enumerate(friend_data):
+		export_to_json(friend['id'], friend['type'], friend['nickname'], friend['user']['username'], friend['user']['discriminator'])
+		print(f"{count} - Friend {friend['user']['username']}#{friend['user']['discriminator']} Exported To JSON File")
+	with io.open('Exported/Exported.json', 'a', encoding='utf-8') as f:
+		f.write(str(json.dumps(Output, indent=4)))
+
+sort_friends()
---
+++ FriendExporter/readme.md
@@ -0,0 +1,14 @@
+# ExportMe
+
+ExportMe Is A Python Tool Made To Export Your Discord Friends With Ease
+## Installation
+
+Download Python From [Here](https://www.python.org/downloads/).
+
+## Usage
+
+```python
+1. Place Your Discord Token In The config.json File 
+2. Run The Command python3 Export.py
+3. Check The File Located In Exported/Exported.json!
+```
